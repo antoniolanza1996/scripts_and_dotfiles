0d9137deca104ce39f6c8f15f920d7a7fda007ef
Antonio Lanza <antoniolanza1996@gmail.com>
2023-09-05 12:27:10 +0200

    changed dotfiles

--- dotfiles/brew.sh
+++ dotfiles/brew.sh
@@ -7,3 +7,4 @@ brew install screenfetch
 brew install wget
 brew install coreutils # in order to use `gdate` command in `remind` script
 brew install the_silver_searcher # in order to use `ag` command in `search` script
+brew install MonitorControl # https://github.com/MonitorControl/MonitorControl
